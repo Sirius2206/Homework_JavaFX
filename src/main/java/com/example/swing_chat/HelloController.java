cb41bb7820f7351b37c623a6b641d32d9399f558
sirius2206 <sergei_galkin@inbox.ru>
2021-11-17 16:44:37 +0500

    Lesson4__JavaFX

---
+++ src/main/java/com/example/swing_chat/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.swing_chat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    TextArea mainTextArea;
+
+    @FXML
+    TextField messageField;
+
+    public void onSendButtonClick(ActionEvent actionEvent) {
+        String message;
+        mainTextArea.appendText("You: \n");
+        message = messageField.getText();
+        mainTextArea.appendText(message + "\n");
+        messageField.setText("");
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.swing_chat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.swing_chat to javafx.fxml;
+    exports com.example.swing_chat;
+}
